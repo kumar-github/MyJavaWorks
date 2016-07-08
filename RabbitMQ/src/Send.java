a44a2538850e93f4398d1d6aef076709d599a804
Saravana Kumar M <kumar9885035454@gmail.com>
2016-07-08 07:22:32 +0000

    Removed some AfterBurner projects since it is available in other repository. Also added a RabbitMQ project.

---
+++ RabbitMQ/src/Send.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send
+{
+	private static final String QUEUE_NAME = "hello";
+	
+	public static void main(String[] args)
+	{
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try {
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+			
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "This is to test.";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+			System.out.println("X sent : " + message + "");
+			
+			channel.close();
+			connection.close();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		} catch (TimeoutException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
